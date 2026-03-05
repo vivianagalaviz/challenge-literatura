1bbe5af339afeaa1ac0e4371f08e667d2950c9e8
Viviana Galaviz <viviana.galaviz3096@alumnos.udg.mx>
2026-03-05 14:04:39 -0600

    agregar DTO DatosLibro para mapear información de libros desde Gutendex

--- src/main/java/com/alura/literatura/dto/DatosLibro.java
+++ src/main/java/com/alura/literatura/dto/DatosLibro.java
@@ -1,4 +1,42 @@
 package com.alura.literatura.dto;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
 public class DatosLibro {
+    @JsonAlias("title")
+    private String titulo;
+
+    @JsonAlias("authors")
+    private List<DatosAutor> autores;
+
+    @JsonAlias("languages")
+    private List<String> idiomas;
+
+    @JsonAlias("download_count")
+    private Integer descargas;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public List<DatosAutor> getAutores() {
+        return autores;
+    }
+
+    public List<String> getIdiomas() {
+        return idiomas;
+    }
+
+    public Integer getDescargas() {
+        return descargas;
+    }
+
+    @Override
+    public String toString() {
+        return "\nTítulo: " + titulo +
+                "\nAutor: " + autores +
+                "\nIdioma: " + idiomas +
+                "\nDescargas: " + descargas;
+    }
 }
