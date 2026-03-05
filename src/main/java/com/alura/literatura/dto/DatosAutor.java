fce04bab5da650cb6e7352a8156c21b7b146f9c7
Viviana Galaviz <viviana.galaviz3096@alumnos.udg.mx>
2026-03-05 14:01:54 -0600

    agregar DTO DatosAutor para mapear autores del JSON de Gutendex

---
+++ src/main/java/com/alura/literatura/dto/DatosAutor.java
@@ -0,0 +1,34 @@
+package com.alura.literatura.dto;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DatosAutor {
+    @JsonAlias("name")
+    private String nombre;
+
+    @JsonAlias("birth_year")
+    private Integer nacimiento;
+
+    @JsonAlias("death_year")
+    private Integer fallecimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getNacimiento() {
+        return nacimiento;
+    }
+
+    public Integer getFallecimiento() {
+        return fallecimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor: " + nombre +
+                " | Nacimiento: " + nacimiento +
+                " | Fallecimiento: " + fallecimiento;
+    }
+}
---
+++ src/main/java/com/alura/literatura/dto/DatosLibro.java
@@ -0,0 +1,4 @@
+package com.alura.literatura.dto;
+
+public class DatosLibro {
+}
